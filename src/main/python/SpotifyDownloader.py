8ec26e28eb79b4418250b93ba2f2bedd740d9dab
Coşkun Yerli <c.yerli@emakina.com.tr>
2020-11-13 01:17:08 +0300

    Add Toast widget

--- src/main/python/SpotifyDownloader.py
+++ src/main/python/SpotifyDownloader.py
@@ -1,12 +1,20 @@
+import os
+
 from fbs_runtime.application_context.PySide2 import ApplicationContext
 from mainWindow import MainWindow
 
 import sys
 
+from widgets.toast import Toast
+
 if __name__ == '__main__':
-	appctxt = ApplicationContext()  # 1. Instantiate ApplicationContext
+	fbs = ApplicationContext()  # 1. Instantiate ApplicationContext
 	# setup ui
+
+	Toast.settings['iconsPath'] = fbs.get_resource(os.path.join('icons', 'toast'))
 	mainWindow = MainWindow()
+	Toast.setWidget(mainWindow)
+
 	mainWindow.show()
-	exit_code = appctxt.app.exec_()  # 2. Invoke appctxt.app.exec_()
+	exit_code = fbs.app.exec_()  # 2. Invoke appctxt.app.exec_()
 	sys.exit(exit_code)
