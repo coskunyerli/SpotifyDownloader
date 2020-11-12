85c806c5fb952346a5651e56ef770d1768c43fa0
Coşkun Yerli <c.yerli@emakina.com.tr>
2020-11-13 01:14:53 +0300

    Add remove song feature, and basic improvements

--- src/main/python/widgets/dragDropListView.py
+++ src/main/python/widgets/dragDropListView.py
@@ -1,4 +1,5 @@
 from PySide2 import QtWidgets, QtCore
+from widgets.toast import Toast
 
 
 class DragDropListView(QtWidgets.QListView):
@@ -10,16 +11,27 @@ class DragDropListView(QtWidgets.QListView):
 		self.setAcceptDrops(True)
 		self.setDragEnabled(True)
 		self.setDragDropMode(QtWidgets.QAbstractItemView.DragDrop)
+		self.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+		self.customContextMenuRequested.connect(self.__showContextMenu)
+
+		self.initShortcuts()
+
+
+	def initShortcuts(self):
+		self.deleteItemShotcut = QtWidgets.QShortcut(self)
+		self.deleteItemShotcut.activated.connect(self.__deleteSelectedItems)
 
 
 	def dropEvent(self, event):
 		try:
 			urls = event.mimeData().text().split('\n')
-			if urls:
-				self.dropped.emit(urls)
+			model = self.model()
+			if model is not None:
+				notContainsUrl = list(filter(lambda url: model.contains(lambda songs: songs.getUrl() == url) is False, urls))
+				if notContainsUrl:
+					self.dropped.emit(notContainsUrl)
 		except Exception as e:
 			print(e)
-
 
 
 	def dragEnterEvent(self, event):
@@ -29,3 +41,21 @@ class DragDropListView(QtWidgets.QListView):
 
 	def dragMoveEvent(self, event):
 		event.acceptProposedAction()
+
+
+	def __showContextMenu(self, point):
+		globalPos = self.mapToGlobal(point)
+		contextMenu = QtWidgets.QMenu()
+		delete = contextMenu.addAction('Remove')
+
+		action = contextMenu.exec_(globalPos)
+
+		if action == delete:
+			self.__deleteSelectedItems(self.selectedIndexes())
+
+
+	def __deleteSelectedItems(self, indexes):
+		model = self.model()
+		result = model.deleteRows(indexes)
+		if result is False:
+			Toast.error('Delete Song Item Error', 'Song is not deleted successfully')
