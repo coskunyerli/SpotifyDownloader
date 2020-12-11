a165d0f0f6f02734b8bb5f8dfc5c7998b528c1b0
Coşkun Yerli <c.yerli@emakina.com.tr>
2020-12-11 19:10:15 +0300

    exception handling is done for image.py

--- src/main/python/model/image.py
+++ src/main/python/model/image.py
@@ -1,17 +1,23 @@
 import PySide2.QtGui as QtGui
 from urllib.request import urlopen
+import ssl
+import log
 
 
 class Image(object):
 	def __init__(self, url):
 		self.url = url
 		if url:
-			byte = urlopen(url).read()
-			image = QtGui.QImage()
-			image.loadFromData(byte)
-			if image:
-				self.data = image
-			else:
-				self.data = QtGui.QImage()
+			try:
+				gcontext = ssl.SSLContext()
+				byte = urlopen(url, context = gcontext).read()
+				image = QtGui.QImage()
+				image.loadFromData(byte)
+				if image:
+					self.data = image
+				else:
+					self.data = QtGui.QImage()
+			except Exception as e:
+				log.error(f'Image of song is not loaded successfully. Exception is {e}')
 		else:
 			self.data = QtGui.QImage()
